ce819445e9deb9b57e74ec784c4bf434c24a020f
parthahere001 <parthahere001.com>
2023-06-20 15:41:32 +0530

    [ADD] Function for updating movie records

--- main.go
+++ main.go
@@ -70,6 +70,28 @@ func createMovie(w http.ResponseWriter, r *http.Request){
 }
 
 
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	// set json content type
+	w.Header().Set("Content-Type","application/json")
+	// params
+	params := mux.Vars(r)
+	// loop over the movies, range
+	// delete the movie with the i.d. that you've sent
+	// add a new movie - the movie that we sent in the body of postman
+
+	for index, item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
 
 func main(){
 	r := mux.NewRouter()
